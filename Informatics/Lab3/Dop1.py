66035357364ff43b3491760d9abef3415fe5d761
DmitriyLesnoy <vasymedov@gmail.com>
2024-10-21 20:50:17 +0300

    opd 1 - test infa 3 - checked 100

--- Informatics/Lab3/Dop1.py
+++ Informatics/Lab3/Dop1.py
@@ -1,16 +1,51 @@
 import re
+
 inp = input()
-sent=inp
-words = re.finditer(r"\S+",inp)
-wordsl = []
-result=""
+# inp = "gIT . Git ... hub ! ; git a ; hub . hub hub. hub    git hub ! git hub & hub git git git hub hub git"
+# inp = "Довольно распространённая ошибка ошибка ошибка – это лишний повтор повтор слова слова слова. Смешно, не не не не не правда ли? Не нужно портить хор хоровод."
+words = re.split(r"(\W+)", inp)
 print(words)
-# for i in range(len(words)+1): # len не прменим к re
-#     if i!=len(words):
-#         wordsl.append([words.group(0),words.start(0),words.end(0)])
-#     if i!=0 and wordsl!=[]:
-#         if wordsl[i-1][0]!=wordsl[i][0]:
-#             result+=inp[wordsl[i-1][1]:wordsl[i][2]]
-#         else:
-#             result += inp[wordsl[i][1] : wordsl[i][2]]
-# print(wordsl)
+words1 = [words[0]]
+
+i = 1
+l = len(words)
+while i < len(words) - 1:
+    words1.append(words[i])
+
+    if words[i-1] != words[i+1]:
+        # words1.append(words[i])
+        # i += 1
+        words1.append(words[i+1])
+        # i +6= 2
+    # else:
+        # words1.append(words[i])
+    i += 2
+print(words1)
+# i = 1
+# words1 = [words[0],words[1]]
+# for i in range(1, l):
+#     if re.match("\w", words[i]) is not None:
+#         for w in words1[::-1]:
+#             if len(w)!=1:
+#                 if words[i] == w:
+#                    continue
+#     else:
+#         words1.append(words[i])
+#     pass
+# words1.append(words[-2])
+# words1.append(words[-1])
+
+while "" in words1:
+    words1.remove("")
+i = 0
+while i < len(words1) - 1:
+    if (
+        re.match("\W", words1[i]) is not None
+        and re.match("\W", words1[i + 1]) is not None
+    ):
+        words1.pop(i)
+    else:
+        i += 1
+
+print(inp)
+print("".join(words1))
---
+++ OPD/Lab1/345.sh
@@ -0,0 +1,123 @@
+cd lab0
+
+touch cherrim4
+echo 'Возможности Overland=6 Surface=4 Jump=4 Power=1
+Intelligence=4 Bloom=0' > cherrim4
+
+touch drilbur6
+echo 'Возможности Overland=4 Surface=2
+Burrow=6 Jump=2 Power=2 Intelligence=4' > drilbur6
+
+mkdir gengar6
+cd gengar6
+touch rhyperior
+echo 'Живет
+Mountaain' > rhyperior
+mkdir skiploom
+mkdir gloom
+touch shelgon
+echo 'satk=6 sdef=5 spd=5' > shelgon
+touch togepi
+echo 'Живет Forest' > togepi
+touch masquerain
+echo 'Тип покемона BUG FLYING' > masquerain
+
+cd ..
+mkdir herdier5
+cd herdier5
+touch oddish
+echo 'Тип диеты
+Phototroph' > oddish
+touch gastly
+echo 'Возможности Sky=8 Power=1 Intelligence=3
+Invisibility=0 Phasing=0' > gastly
+touch ledyba
+echo 'Тип диеты Herbivore' > ledyba
+touch roggenrola
+echo 'Тип
+диеты Nerravore' > roggenrola
+
+cd ..
+touch shedinja1
+echo 'Способности Leech Life Sand-Attach Fury
+Swipes Mind Reader Spite Confuse Ray Shadow Sneak Grudge Heal Block
+Shadow Ball' > shedinja1
+
+mkdir watchog3
+cd watchog3
+mkdir gorebyss
+mkdir purugly
+touch litwick
+echo 'Способности Astonish Ember Minimize Smog Fire
+Spin Confuse ray Might Shade Will-0-Wils Flame Burts Impression Hex
+Memento Inferno Curse Shadow Ball Pain Split Overheat' > litwick
+touch mandibuzz
+echo 'Живет
+Desert Mountain' > mandibuzz
+mkdir munna
+mkdir fearow
+cd ..
+
+
+chmod 400 cherrim4
+chmod u=rw,g=r,o-rwx drilbur6
+
+cd gengar6
+chmod =-w-r-x,g=rw rhyperior
+chmod =x,u+r,g+rw,o+w skiploom
+chmod uo+w,g+wr gloom
+chmod =-x,ug=w,u+r shelgon
+chmod u=rw,g=w,o=-r-w-x togepi
+chmod 644 masquerain
+cd ..
+chmod 755 gengar6
+
+cd ./herdier5
+chmod =-x,ug+w,u+r,o-r oddish
+chmod 046 gastly
+chmod -rwx,u+r ledyba
+chmod u=rw,g=w,o=w roggenrola
+cd ..
+chmod 571 herdier5
+chmod 404 shedinja1
+cd watchog3
+chmod ugo=rwx,g-r gorebyss
+chmod ugo=w,uo+x,g+r purugly
+chmod -rwx,o+r litwick
+chmod ug=,o=+rw mandibuzz
+chmod uo=wx,g=rw munna
+chmod ug=rwx,g-w,o=g-x fearow
+cd ..
+chmod u=rx,g=w,o=r watchog3
+
+chmod u+rwx ./watchog3/litwick
+chmod u+rwx ./watchog3/litwick
+chmod 004 ./watchog3/litwick
+ln ./drilbur6 ./gengar6/togepidrilbur
+cp ./drilbur6 ./watchog3/fearow
+ln -s ./gengar6 ./Copy_97
+ln -s ./shedinja1 ./gengar6/rhyperiorshedinja
+cat shedinja1 > ./watchog3/litwickshedinja
+chmod -R u=rx ./watchog3
+chmod -R u=rwx ./gengar6/gloom
+cp -R ./watchog3 ./gengar6/gloom
+cd ./gengar6/gloom/watchog3
+chmod ugo=rwx,g-r gorebyss
+chmod ugo=w,uo+x,g+r purugly
+chmod -rwx,o+r litwick
+chmod ug=,o=+rw mandibuzz
+chmod uo=wx,g=rw munna
+chmod ug=rwx,g-w,o=g-x fearow
+cd ..
+chmod u=rx,g=w,o=r watchog3
+cd ..
+chmod uo+w,g+wr gloom
+cd ../watchog3/
+chmod ugo=rwx,g-r gorebyss
+chmod ugo=w,uo+x,g+r purugly
+chmod -rwx,o+r litwick
+chmod ug=,o=+rw mandibuzz
+chmod uo=wx,g=rw munna
+chmod ug=rwx,g-w,o=g-x fearow
+cd ..
+chmod u=rx,g=w,o=r watchog3
