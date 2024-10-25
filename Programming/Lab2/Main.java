1ad5d7feccc4200acf65a76fc93f0e14b49105c1
DmitriyLesnoy <vasymedov@gmail.com>
2024-10-25 19:59:13 +0300

    vault backup: 2024-10-25 19:59:12

--- Programming/Lab2/Main.java
+++ Programming/Lab2/Main.java
@@ -1,8 +1,14 @@
-import java.util.Scanner;
+import org.ITMO.Pokemon;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
+        Battle b = new Battle();
+        Pokemon p1 = new Pokemon("Чужой", 1);
+        Pokemon p2 = new Pokemon("Хищник", 1);
+        b.addAlly(p1);
+        b.addFoe(p2);
+        b.go();
 
     }
 }
