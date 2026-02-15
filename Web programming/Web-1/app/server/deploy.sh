fb8d011f7a3dd62584671767a904f9ba944dcaed
Akim Lineiskiy <vasymedov@gmail.com>
2026-02-15 22:34:10 +0300

    web lab 1

--- Web programming/Web-1/app/server/deploy.sh
+++
@@ -1,4 +0,0 @@
-killall httpd
-killall java
-httpd -f ~/web1/httpd.conf -k restart
-java -jar -DFCGI_PORT=9000 ./httpd-root/fcgi-bin/server.jar
--- Web programming/Web-2/app/src/main/java/web/taghandler/Rule.java
+++
@@ -1,4 +0,0 @@
-package web.taghandler;
-
-public record Rule(String type, String value, String message) {
-}
