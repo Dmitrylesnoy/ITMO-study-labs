7e76a32c727b67f74e31ed2f5a77dfef552be15d
Akim Lineiskiy <vasymedov@gmail.com>
2026-02-19 17:49:12 +0300

    Lab 1 complete (report)

--- Computation math/Comp_lab_1/compute.py
+++ Computation math/Comp_lab_1/compute.py
@@ -143,7 +143,7 @@ def calc_gauss_seidel(A, X, B, m=10, e=1e-2):
                 k = k + 1
             else:
                 print("Итерации расходятся")
-                exit(0)
+                break
 
 '''
 • Проверка диагонального преобладания (в случае, если диагональное 
@@ -153,57 +153,63 @@ def calc_gauss_seidel(A, X, B, m=10, e=1e-2):
 водить соответствующее сообщение. 
 • Вывод нормы матрицы (любой, на Ваш выбор), 
 • Вывод вектора  погрешностей: |𝑥𝑖(𝑘) − 𝑥𝑖(𝑘−1)|'''
+while 1:
+    n = validate_int_input("Введите размерность матрицы:", min_value=1)
 
-n = validate_int_input("Введите размерность матрицы:", min_value=1)
+    print("Выберите способ ввода матрицы: ")
+    print("1 - Ввести вручную")
+    print("2 - Сгенерировать случайную матрицу")
+    print("3 - прочитать данные из файла")
+    print("4 - выход")
+    
+    choice = validate_int_input("Ваш выбор: ", 1, 4)
 
-print("Выберите способ ввода матрицы: ")
-print("1 - Ввести вручную")
-print("2 - Сгенерировать случайную матрицу")
-print("3 - прочитать данные из файла")
-choice = validate_int_input("Ваш выбор: ", 1, 3)
+    m = 10
+    e = 0.01
 
-if choice == 1:
-    # [[2,2,10],
-    # [10,1,1],
-    # [2,10,1]]
-    A = np.zeros((n, n), float)
-    print("Построчно введите матрицу коэффициентов A")
-    for i in range(n):
-        A[i] = validate_vector(f"A[{i+1}]: ", n)
+    if choice == 1:
+        # [[2,2,10],
+        # [10,1,1],
+        # [2,10,1]]
+        A = np.zeros((n, n), float)
+        print("Построчно введите матрицу коэффициентов A")
+        for i in range(n):
+            A[i] = validate_vector(f"A[{i+1}]: ", n)
+            
+        # [14,12,13]
+        print("Введите матрицу свободных членов B")
+        B = validate_vector("B: ", n)
+        print("Введите матрицу неизвестных X")
+        X = validate_vector("X: ", n)
+        print("Введите точность")
+        e = validate_vector("e: ", 1)
         
-    # [14,12,13]
-    print("Введите матрицу свободных членов B")
-    B = validate_vector("B: ", n)
-    print("Введите матрицу неизвестных X")
-    X = validate_vector("X: ", n)
-    
-elif choice == 2:
-    A = np.random.rand(n, n) * 10 - 5
-    B = np.random.rand(n) * 20 - 10
-    X = np.random.rand(n) * 20 - 10
-    print("Сгенерированная матрица A:")
-    print(A)
-    print("Сгенерированный вектор B:")
-    print(B)
-    print("Сгенерированный вектор X:")
-    print(X)
-    
-elif choice == 3:
-    A, B, X = parse_json_matrix(input("Введите имя файла: "))
-    print("Сгенерированная матрица A:")
-    print(A)
-    print("Сгенерированный вектор B:")
-    print(B)
-    print("Сгенерированный вектор X:")
-    print(X)
-    
-    
-else:
-    print("Неверный выбор. Выход.")
-    exit(1)
+        
+    elif choice == 2:
+        A = np.random.rand(n, n) * 10 - 5
+        B = np.random.rand(n) * 20 - 10
+        X = np.random.rand(n) * 20 - 10
+        print("Сгенерированная матрица A:")
+        print(A)
+        print("Сгенерированный вектор B:")
+        print(B)
+        print("Сгенерированный вектор X:")
+        print(X)
+        
+    elif choice == 3:
+        A, B, X = parse_json_matrix(input("Введите имя файла: "))
+        print("Сгенерированная матрица A:")
+        print(A)
+        print("Сгенерированный вектор B:")
+        print(B)
+        print("Сгенерированный вектор X:")
+        print(X)
+    elif choice == 4:
+        exit()       
+    else:
+        print("Неверный выбор. Выход.")
+        exit(1)
 
-m = 10
-e = 0.01
 
-A, B = transform2diagonal(A, B)
-calc_gauss_seidel(A, X, B, m, e)
+    A, B = transform2diagonal(A, B)
+    calc_gauss_seidel(A, X, B, m, e)
