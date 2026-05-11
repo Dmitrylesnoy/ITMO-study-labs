93be06d6f2115d324d28cd149b2c343cecf4c06e
Akim Lineiskiy <vasymedov@gmail.com>
2026-05-11 12:26:30 +0300

    m68k wrench tests passed

--- Computer Architecture/Lab_3_m68k/main.s
+++ Computer Architecture/Lab_3_m68k/main.s
@@ -1,93 +1,118 @@
     .text
     .org     0x100
 _start:
-    movea.l  stack_top, A7
-    movea.l  (A7), A7        ; Установка SP
+    movea.l  input_addr, A3
+
     movea.l  input_addr, A0
     movea.l  (A0), A0        ; A0 -> порт ввода
     movea.l  output_addr, A1
     movea.l  (A1), A1        ; A1 -> порт вывода
-    movea.l  token_addr, A2
-    movea.l  (A2), A2        ; A2 -> начало списка токенов
-    
+
     clr.l    D3              ; Флаг: 1 если мы в процессе сборки числа
     clr.l    D4              ; Текущее собираемое число
 
 read_char:
-    move.b   (A0), D0        ; Читаем 1 байт из ввода
-    cmp.b    const_endl, D0  ; Проверка на конец строки
-    beq      finalize_and_main
-    cmp.b    const_space, D0 ; Проверка на пробел
+    move.b   0, D0
+    move.b   (A0), D0       ; Читаем 1 байт из ввода
+    cmp.b    0x0A, D0       ; Проверка на конец строки
+    beq      start_calc
+    cmp.b    ' ', D0        ; Проверка на пробел
     beq      handle_space
 
     ; Если это оператор, то сохраняем его как токен
-    cmp.b    const_plus, D0         ; '+'
+    cmp.b    '+', D0          ; '+'
     beq      save_op
-    cmp.b    const_minus, D0        ; '-'
+    cmp.b    '-', D0          ; '-'
     beq      save_op
-    cmp.b    const_mul, D0          ; '*'
+    cmp.b    '*', D0          ; '*'
     beq      save_op
-    cmp.b    const_div, D0          ; '/'
+    cmp.b    '/', D0          ; '/'
     beq      save_op
 
     ; Если цифра
-    sub.b    const_num_char, D0     ; Переводим в цифру 0-9
+    sub.b    '0', D0         ; Переводим в цифру 0-9
     mul.l    10, D4
     add.l    D0, D4
+    cmp.l    1073741824, D4
+    bgt      return_overflow
+    cmp.l    -1073741824, D4
+    blt      return_overflow
     move.l   1, D3           ; Ставим флаг "собираем число"
     jmp      read_char
 
 handle_space:
     cmp.l    1, D3           ; Если до этого собирали число, сохраняем его
     bne      read_char
-    move.l   D4, (A2)+       ; Сохраняем число в токены
+
+    ; Сохраняем число в токены
+    add.l    4, 24(A3)
+    movea.l  24(A3), A2
+    move.l   D4, (A2)
+
     clr.l    D4              ; Сброс сборщика
     clr.l    D3              ; Сброс флага
     jmp      read_char
 
 save_op:
-    move.l   D0, (A2)+       ; Сохраняем ASCII код оператора как токен
+    ; Сохраняем ASCII код оператора как токен
+    add.l    4, 24(A3)
+    movea.l  24(A3), A2
+    move.l   D0, (A2)
     jmp      read_char
 
-finalize_and_main:
-    cmp.l    1, D3           ; Добавляем последнее число, если оно было
-    bne      start_calc
-    move.l   D4, (A2)+
-
 start_calc:
-    move.l   0, (A2)         ; Маркер конца токенов
-    movea.l  token_addr, A3
-    movea.l  (A3), A3        ; A3 итерирует по токенам
-    movea.l  A7, A6          ; Используем A6 как указатель стека калькулятора
+    move.l  24(A3), D0
+    sub.l   8(A3), D0
+    beq     return_err
+    cmp.l   12, D0
+    blt     return_err
+
+    move.l  24(A3), 28(A3) ; обвновляем адрес конца списка токенов
+    move.l   8(A3), 24(A3)  ; устанавливаем указатель на начало списка токенов
+
+    move.l  12(A3), 16(A3) ; обновляем указатель стека наверх
 
 main_loop:
-    move.l   (A3)+, D0       ; Берем токен
-    cmp.l    0, D0           ; Конец?
-    beq      main_loop_end
+    move.l  24(A3), D0
+    sub.l   28(A3), D0
+    beq     main_loop_end
 
-    ; Проверяем, оператор это или число
-    cmp.l    const_num_char, D0
-    bge      push_num
+    add.l    4, 24(A3)
+    movea.l  24(A3), A2
+    move.l   (A2), D0
 
-    ; Это оператор. Нужно минимум 2 числа в стеке.
-    ; (A7 - A6) / 4 — количество элементов.
-    move.l   A7, D1
-    sub.l    A6, D1
-    cmp.l    8, D1           ; Меньше 8 байт (2-х лонгов)?
+    cmp.l    '+', D0           ; '+'
+    beq      check_nums_stack
+    cmp.l    '-', D0           ; '-'
+    beq      check_nums_stack
+    cmp.l    '*', D0           ; '*'
+    beq      check_nums_stack
+    cmp.l    '/', D0           ; '/'
+    beq      check_nums_stack
+    jmp      push_num
+
+check_nums_stack:
+    move.l   16(A3), D1
+    sub.l    12(A3), D1
+    cmp.l    8, D1           ; Меньше 2 чисел?
     blt      return_err
 
-    move.l   -(A6), D2       ; b
-    move.l   -(A6), D1       ; a
+    movea.l  16(A3), A2
+    move.l   (A2), D2        ; b
+    sub.l    4, 16(A3)
 
-    cmp.l    const_plus, D0         ; '+'
+    movea.l  16(A3), A2
+    move.l   (A2), D1        ; a
+    sub.l    4, 16(A3)
+
+    cmp.l    '+', D0           ; '+'
     beq      do_add
-    cmp.l    const_minus, D0        ; '-'
+    cmp.l    '-', D0           ; '-'
     beq      do_sub
-    cmp.l    const_mul, D0          ; '*'
+    cmp.l    '*', D0           ; '*'
     beq      do_mul
-    cmp.l    const_div, D0          ; '/'
+    cmp.l    '/', D0           ; '/'
     beq      do_div
-    jmp      return_err             ; Неизвестный символ
 
 do_add:
     add.l    D1, D2
@@ -106,21 +131,28 @@ do_div:
     move.l   D1, D2
 
 push_res:
-    move.l   D2, (A6)+
+    add.l    4, 16(A3)
+    movea.l  16(A3), A2
+    move.l   D2, (A2)
     jmp      main_loop
 
 push_num:
-    move.l   D0, (A6)+
+    add.l    4, 16(A3)
+    movea.l  16(A3), A2
+    move.l   D0, (A2)
     jmp      main_loop
 
 main_loop_end:
-    ; В стеке должен остаться ровно один результат
-    move.l   A6, D1
-    sub.l    A7, D1
+    move.l   16(A3), D1
+    sub.l    12(A3), D1
     cmp.l    4, D1           ; Должно быть ровно 4 байта
     bne      return_err
 
-    move.l   -(A6), (A1)     ; Вывод результата
+    ; Вывод результата
+    movea.l  16(A3), A2
+    move.l   (A2), D1
+    movea.l  4(A3), A2
+    move.l   D1, (A2)
     halt
 
 return_err:
@@ -128,24 +160,28 @@ return_err:
     halt
 
 return_overflow:
-    move.l   const_overflow, (A1)
+    movea.l   const_overflow, A2
+    move.l    (A2), D1
+    move.l    D1, (A1)
     halt
 
-
     .data
-    .org 0x300
-input_addr:      .word  0x80
-output_addr:     .word  0x84
-stack_top:       .word  0x400
+    .org 0x400
+input_addr:      .word  0x80    ; +0
+output_addr:     .word  0x84    ; +4
+token_addr:      .word  0x500  ; +8
+stack_top:       .word  0x600   ; +12
+stack_ptr:       .word  0x600   ; +16
+stack_acc_prt:   .word  0x0   ; +20
+token_ptr:       .word  0x500  ; +24
+token_end:       .word  0x500   ; +28
 
-const_endl:      .byte  10
-const_space:     .byte  32
-const_overflow:  .word  0xCCCCCCCC
-const_num_char:  .byte  48
+const_endl:      .byte  10      ; +24
+const_space:     .byte  32      ; +28
+const_overflow:  .word  0xCCCCCCCC ; +32
+const_num_char:  .byte  48      ; +36
 
-const_plus:      .byte  43
-const_minus:     .byte  45
-const_mul:       .byte  42
-const_div:       .byte  47
-
-token_addr:      .word  0x600
+const_plus:      .byte  43      ; +40
+const_minus:     .byte  45      ; +44
+const_mul:       .byte  42      ; +48
+const_div:       .byte  47      ; +52
--- Computer Architecture/Lab_3_m68k/test.s
+++ Computer Architecture/Lab_3_m68k/test.s
@@ -3,8 +3,6 @@
 _start:
     movea.l  input_addr, A3
 
-    ; movea.l  stack_top, A7
-    ; movea.l  (A7), A7        ; Установка SP
     movea.l  input_addr, A0
     movea.l  (A0), A0        ; A0 -> порт ввода
     movea.l  output_addr, A1
@@ -17,7 +15,7 @@ read_char:
     move.b   0, D0
     move.b   (A0), D0       ; Читаем 1 байт из ввода
     cmp.b    0x0A, D0       ; Проверка на конец строки
-    beq      finalize_and_main
+    beq      start_calc
     cmp.b    ' ', D0        ; Проверка на пробел
     beq      handle_space
 
@@ -35,15 +33,20 @@ read_char:
     sub.b    '0', D0         ; Переводим в цифру 0-9
     mul.l    10, D4
     add.l    D0, D4
+    cmp.l    1073741824, D4
+    bgt      return_overflow
+    cmp.l    -1073741824, D4
+    blt      return_overflow
     move.l   1, D3           ; Ставим флаг "собираем число"
     jmp      read_char
 
 handle_space:
     cmp.l    1, D3           ; Если до этого собирали число, сохраняем его
     bne      read_char
-    ; move.l   D4, (A2)+       ; Сохраняем число в токены
-    movea.l  24(A3), A2
+
+    ; Сохраняем число в токены
     add.l    4, 24(A3)
+    movea.l  24(A3), A2
     move.l   D4, (A2)
 
     clr.l    D4              ; Сброс сборщика
@@ -51,39 +54,32 @@ handle_space:
     jmp      read_char
 
 save_op:
-    ; move.l   D0, (A2)+       ; Сохраняем ASCII код оператора как токен
-    movea.l  24(A3), A2
+    ; Сохраняем ASCII код оператора как токен
     add.l    4, 24(A3)
+    movea.l  24(A3), A2
     move.l   D0, (A2)
     jmp      read_char
 
-finalize_and_main:
-    cmp.l    1, D3           ; Добавляем последнее число, если оно было
-    movea.l  8(A3), A2
-    movea.l  24(A3), A2
-    bne      start_calc
-
-    ; move.l   D4, (A2)+
-    movea.l  24(A3), A2
-    add.l    4, 24(A3)
-    move.l   D4, (A2)
-    
-
 start_calc:
+    move.l  24(A3), D0
+    sub.l   8(A3), D0
+    beq     return_err
+    cmp.l   12, D0
+    blt     return_err
+
     move.l  24(A3), 28(A3) ; обвновляем адрес конца списка токенов
     move.l   8(A3), 24(A3)  ; устанавливаем указатель на начало списка токенов
 
     move.l  12(A3), 16(A3) ; обновляем указатель стека наверх
-    move.l  12(A3), 20(A3)
 
 main_loop:
     move.l  24(A3), D0
     sub.l   28(A3), D0
     beq     main_loop_end
 
+    add.l    4, 24(A3)
     movea.l  24(A3), A2
     move.l   (A2), D0
-    add.l    4, 24(A3)
 
     cmp.l    '+', D0           ; '+'
     beq      check_nums_stack
@@ -117,7 +113,6 @@ check_nums_stack:
     beq      do_mul
     cmp.l    '/', D0           ; '/'
     beq      do_div
-    jmp      return_err        ; Неизвестный символ
 
 do_add:
     add.l    D1, D2
@@ -148,17 +143,16 @@ push_num:
     jmp      main_loop
 
 main_loop_end:
-    ; В стеке должен остаться ровно один результат
-    ; move.l   A6, D1
-    ; move.l   16(A3), D1
-    ; sub.l    A7, D1
-    ; sub.l    12(A3), D1
-    ; cmp.l    4, D1           ; Должно быть ровно 4 байта
-    ; bne      return_err
+    move.l   16(A3), D1
+    sub.l    12(A3), D1
+    cmp.l    4, D1           ; Должно быть ровно 4 байта
+    bne      return_err
 
     ; Вывод результата
-    movea.l    4(A3), A2
-    move.l   D2, (A2)
+    movea.l  16(A3), A2
+    move.l   (A2), D1
+    movea.l  4(A3), A2
+    move.l   D1, (A2)
     halt
 
 return_err:
@@ -166,7 +160,9 @@ return_err:
     halt
 
 return_overflow:
-    move.l   const_overflow, (A1)
+    movea.l   const_overflow, A2
+    move.l    (A2), D1
+    move.l    D1, (A1)
     halt
 
     .data
@@ -174,10 +170,10 @@ return_overflow:
 input_addr:      .word  0x80    ; +0
 output_addr:     .word  0x84    ; +4
 token_addr:      .word  0x500  ; +8
-stack_top:       .word  0x700   ; +12
+stack_top:       .word  0x600   ; +12
 stack_ptr:       .word  0x600   ; +16
-stack_acc_prt:   .word  0x600   ; +20
-token_ptr:       .word  0x600  ; +24
+stack_acc_prt:   .word  0x0   ; +20
+token_ptr:       .word  0x500  ; +24
 token_end:       .word  0x500   ; +28
 
 const_endl:      .byte  10      ; +24
